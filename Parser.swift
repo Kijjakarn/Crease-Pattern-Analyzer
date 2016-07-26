3b2a1bd40cd794c7d3796c2a137442746ca348b1
Kijjakarn Praditukrit <kijjakarn_boss@hotmail.com>
2016-07-26 00:17:11 -0400

    Parser fully implemented

--- Parser.swift
+++ Parser.swift
@@ -115,12 +115,14 @@ func parseInt(inout str: String) -> Int? {
 }
 
 func isDigit(digit: Character) -> Bool {
-    switch digit {
-    case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
-        return true
-    default:
-        return false
-    }
+    return "0" <= digit && digit <= "9"
+}
+
+func isAlphanumeric(char: Character) -> Bool {
+    return isDigit(char)
+        || "a" <= char && char <= "z"
+        || "A" <= char && char <= "Z"
+        || char == "_"
 }
 
 func toNumber(digit: Character) -> Int? {
@@ -138,4 +140,227 @@ func toNumber(digit: Character) -> Int? {
     default: break
     }
     return nil
+}
+
+enum ParseError: ErrorType {
+    case NotEnoughOperands
+    case TooManyOperands
+    case UnbalancedParenthesis
+    case InvalidNumber
+    case InvalidOperation
+    case ParseStackFailure
+    case EmptyExpression
+}
+
+class ShuntingYard {
+    var operands:  [Double]
+    var operators: [String]
+
+    // If the last thing pushed on a stack is an operator
+    // If true, "-" becomes a unary negation operator "M"
+    var operatorPushed = true
+
+    init() {
+        operands  = []
+        operators = []
+    }
+
+    // Take an operator popped from the operators stack, apply it to the
+    // appropriate number of arguments from the operands stack, and push the
+    // result back into the operands stack
+    func apply(operatorName: String) throws {
+        switch operatorName {
+
+        // Binary operators
+        case "^", "*", "/", "+", "-":
+            let result: Double
+            guard let operand2 = operands.popLast() else {
+                throw ParseError.NotEnoughOperands
+            }
+            guard let operand1 = operands.popLast() else {
+                throw ParseError.NotEnoughOperands
+            }
+            switch operatorName {
+            case "^":
+                result = pow(operand1, operand2)
+            case "*":
+                result = operand1*operand2
+            case "/":
+                result = operand1/operand2
+            case "+":
+                result = operand1 + operand2
+            case "-":
+                result = operand1 - operand2
+            default:
+                throw ParseError.InvalidOperation
+            }
+            operands.append(result)
+
+        // Unary functions
+        case "sin", "cos", "tan", "ln", "log", "sqrt", "cbrt", "abs", "unary-":
+            let result: Double
+            guard let operand = operands.popLast() else {
+                throw ParseError.NotEnoughOperands
+            }
+            switch operatorName {
+            case "sin":
+                result = sin(operand*π/180)
+            case "cos":
+                result = cos(operand*π/180)
+            case "tan":
+                result = tan(operand*π/180)
+            case "ln":
+                result = log(operand)
+            case "log":
+                result = log10(operand)
+            case "sqrt":
+                result = sqrt(operand)
+            case "cbrt":
+                result = cbrt(operand)
+            case "abs":
+                result = abs(operand)
+            case "unary-":
+                result = -operand
+            default:
+                throw ParseError.InvalidOperation
+            }
+            operands.append(result)
+
+        default:
+            throw ParseError.InvalidOperation
+        }
+    }
+
+    func parse(str: String) throws -> Double! {
+        if str.isEmpty { throw ParseError.EmptyExpression }
+        var str = str
+        var char: Character
+        while !str.isEmpty {
+            char = str.removeAtIndex(str.startIndex)
+
+            // If it is a number, push it on the operands stack
+            if isDigit(char) || char == "." {
+                str.insert(char, atIndex: str.startIndex)
+                if let parsedDouble = parseDouble(&str) {
+                    operands.append(parsedDouble)
+                }
+                else { throw ParseError.InvalidNumber }
+                operatorPushed = false
+            }
+            // If it is not a number, then it must be an operator
+            else {
+                switch char {
+                case "^", "*", "/", "+", "-":
+                    var char: String = String(char)
+                    // Case where "-" is treated as a unary operator
+                    if char == "-" && operatorPushed { char = "unary-" }
+                    if operators.count == 0 {
+                        operators.append(char)
+                    }
+                    else {
+                        let comparison = compareOperators(char,
+                                operators[operators.count - 1])
+                        if comparison >= 0 {
+                            operators.append(char)
+                        }
+                        else {
+                            guard let operatorName = operators.popLast() else {
+                                throw ParseError.TooManyOperands
+                            }
+                            try apply(operatorName)
+                            operators.append(char)
+                        }
+                    }
+                    operatorPushed = true
+                case "(":
+                    operators.append(String(char))
+                    operatorPushed = true
+
+                // Ignore whitespace
+                case " ":
+                    continue
+
+                // Pop all operators until "(" is found
+                case ")":
+                    while true {
+                        guard let operatorName = operators.popLast() else {
+                            throw ParseError.UnbalancedParenthesis
+                        }
+                        if operatorName == "(" { break }
+                        try apply(operatorName)
+                    }
+                    operatorPushed = false
+
+                // The operator is a function
+                default: 
+                    str.insert(char, atIndex: str.startIndex)
+                    let functionName = readFunctionName(&str)
+
+                    // If a constant "p", "e", or "d" is read
+                    switch functionName {
+                    case "p", "e", "d":
+                        operatorPushed = false
+                        switch functionName {
+                        case "p": operands.append(M_PI)
+                        case "e": operands.append(M_E)
+                        case "d": operands.append(sqrt(2))
+                        default: break
+                        }
+                        continue
+                    default: break
+                    }
+                    // Otherwise, a function name is read
+                    operators.append(functionName)
+                    operatorPushed = true
+                }
+            }
+        }
+        // Clear the stacks
+        while operators.count != 0 {
+            guard let operatorName = operators.popLast() else {
+                throw ParseError.ParseStackFailure
+            }
+            try apply(operatorName)
+        }
+        let double = operands.popLast()
+        if operands.count != 0 || operators.count != 0 {
+            throw ParseError.ParseStackFailure
+        }
+        return double
+    }
+}
+
+func readFunctionName(inout str: String) -> String {
+    var name = ""
+    var char: Character
+    while !str.isEmpty {
+        char = str.removeAtIndex(str.startIndex)
+        if !isAlphanumeric(char) {
+            str.insert(char, atIndex: str.startIndex)
+            break
+        }
+        name += String(char)
+    }
+    return name
+}
+
+func compareOperators(operator1: String, _ operator2: String) -> Int {
+    return precedence(operator1) - precedence(operator2)
+}
+
+func precedence(operatorName: String) -> Int {
+    switch operatorName {
+    case "^":
+        return 50
+    case "*", "/":
+        return 40
+    case "unary-":
+        return 30
+    case "+", "-":
+        return 20
+    case "(":
+        return 10
+    default:
+        return 100
+    }
 }
--- main.swift
+++ main.swift
@@ -4,10 +4,96 @@
 
 var main = ReferenceFinder.singleton
 makeAllPointsAndLines()
-main.inputPoint = PointVector(0.322, 0.322)
+
+var parser = ShuntingYard()
+var isParsed = false
+
+var x = 0.0
+var y = 0.0
+
+while !isParsed {
+    print("x-coordinate: ", terminator: "")
+    let str = readLine(stripNewline: true)
+    do {
+        if let parsed = try parser.parse(str!) {
+            print("x-coordinate is \(parsed)")
+            x = parsed
+            isParsed = true
+        }
+        else {
+            print("Empty string")
+        }
+    }
+    catch ParseError.NotEnoughOperands {
+        print("Not enough operands")
+    }
+    catch ParseError.TooManyOperands {
+        print("Too many operands")
+    }
+    catch ParseError.UnbalancedParenthesis {
+        print("Unbalanced parenthesis")
+    }
+    catch ParseError.InvalidNumber {
+        print("Invalid number")
+    }
+    catch ParseError.InvalidOperation {
+        print("Invalid operation")
+    }
+    catch ParseError.ParseStackFailure {
+        print("Stack failure")
+    }
+    catch ParseError.EmptyExpression {
+        print("Cannot parse empty expression")
+    }
+}
+
+isParsed = false
+while !isParsed {
+    print("y-coordinate: ", terminator: "")
+    let str = readLine(stripNewline: true)
+    do {
+        if let parsed = try parser.parse(str!) {
+            print("y-coordinate is \(parsed)")
+            y = parsed
+            isParsed = true
+        }
+        else {
+            print("Empty string")
+        }
+    }
+    catch ParseError.NotEnoughOperands {
+        print("Not enough operands")
+    }
+    catch ParseError.TooManyOperands {
+        print("Too many operands")
+    }
+    catch ParseError.UnbalancedParenthesis {
+        print("Unbalanced parenthesis")
+    }
+    catch ParseError.InvalidNumber {
+        print("Invalid number")
+    }
+    catch ParseError.InvalidOperation {
+        print("Invalid operation")
+    }
+    catch ParseError.ParseStackFailure {
+        print("Stack failure")
+    }
+    catch ParseError.EmptyExpression {
+        print("Cannot parse empty expression")
+    }
+}
+
+main.inputPoint = PointVector(x, y)
 var matchedPts = matchedPoints()
 print("There are \(matchedPts.count) matched points")
 matchedPts.sortInPlace { $0.distanceError < $1.distanceError }
+
+var i = 1
+for matchedPt in matchedPts {
+    print("\(i): \(matchedPt.point),\t\terror = \(matchedPt.distanceError)")
+    i += 1
+}
 
 print("\nTo get \(main.inputPoint) (error = \(matchedPts[0].distanceError)),")
 printInstructions(point: matchedPts[0])
