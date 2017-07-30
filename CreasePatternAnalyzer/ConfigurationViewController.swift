9da93eaa6eedd1a508f694536a14e2b7cbfff2a0
Kijjakarn Praditukrit <kijjakarn_boss@hotmail.com>
2017-07-30 16:52:33 -0700

    Removed XIB for ConfigurationViewController

--- CreasePatternAnalyzer/ConfigurationViewController.swift
+++ CreasePatternAnalyzer/ConfigurationViewController.swift
@@ -23,11 +23,8 @@ class TextField: NSTextField {
 }
 
 class ConfigurationViewController: NSViewController,
+                                   NSControlTextEditingDelegate,
                                    NSTextFieldDelegate {
-    override var nibName: String? {
-        return "ConfigurationViewController"
-    }
-
     dynamic weak var delegate: ConfigurationViewControllerDelegate!
 
     dynamic var width: Double = 1 {
@@ -49,6 +46,7 @@ class ConfigurationViewController: NSViewController,
             if useSquare {
                 width  = 1
                 height = 1
+                isEditing = false
             }
         }
     }
@@ -56,20 +54,34 @@ class ConfigurationViewController: NSViewController,
     let initializationQueue =
         (NSApplication.shared().delegate as! AppDelegate).initializationQueue
 
-    @IBOutlet weak var widthInput:         TextField!
-    @IBOutlet weak var heightInput:        TextField!
-    @IBOutlet weak var errorMessage:       NSTextField!
-    @IBOutlet weak var reinitializeButton: NSButton!
-    @IBOutlet weak var maxRankSelector:    NSPopUpButton!
+    var widthInput:   TextField!
+    var heightInput:  TextField!
+    var errorMessage: NSTextField!
 
-    // Variables for enabling/disabling the findPoint button
+    // Variables for enabling/disabling the reinitialize button
     dynamic var isWidthValid  = true
     dynamic var isHeightValid = true
+    dynamic var isEditing     = false
 
-    dynamic var isEditing = false
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 
-    @IBAction
-    func updateWidth(_ sender: TextField) {
+    func control(_ control: NSControl, textShouldEndEditing fieldEditor: NSText)
+      -> Bool {
+        print("Should end editing called")
+        if (control === widthInput) {
+            print("Going to update width")
+            updateWidth()
+        }
+        if (control === heightInput) {
+            print("Going to update height")
+            updateHeight()
+        }
+        return true
+    }
+
+    func updateWidth() {
         let parsedWidth = Parser.parsedString(from: widthInput.stringValue)
         isEditing = false
         if parsedWidth.success {
@@ -88,8 +100,7 @@ class ConfigurationViewController: NSViewController,
         }
     }
 
-    @IBAction
-    func updateHeight(_ sender: TextField) {
+    func updateHeight() {
         let parsedHeight = Parser.parsedString(from: heightInput.stringValue)
         isEditing = false
         if parsedHeight.success {
@@ -108,68 +119,217 @@ class ConfigurationViewController: NSViewController,
         }
     }
 
-    @IBAction
     func setMaxRank(_ sender: NSPopUpButton) {
-        main.maxRank = maxRankSelector.indexOfSelectedItem + 1
-        Swift.print(main.maxRank)
+        main.maxRank = sender.indexOfSelectedItem + 1
     }
 
-    @IBAction
-    func reinitialize(_ sender: NSView) {
+    func reinitialize() {
         delegate.reinitialize()
     }
 
-    @IBAction
-    func toggleAxiom1(_ sender: NSButton) {
-        toggle(axiom: 1, state: sender.state)
-    }
-
-    @IBAction
-    func toggleAxiom2(_ sender: NSButton) {
-        toggle(axiom: 2, state: sender.state)
-    }
-
-    @IBAction
-    func toggleAxiom3(_ sender: NSButton) {
-        toggle(axiom: 3, state: sender.state)
-    }
-
-    @IBAction
-    func toggleAxiom4(_ sender: NSButton) {
-        toggle(axiom: 4, state: sender.state)
-    }
-
-    @IBAction
-    func toggleAxiom5(_ sender: NSButton) {
-        toggle(axiom: 5, state: sender.state)
-    }
-
-    @IBAction
-    func toggleAxiom6(_ sender: NSButton) {
-        toggle(axiom: 6, state: sender.state)
-    }
-
-    @IBAction
-    func toggleAxiom7(_ sender: NSButton) {
-        toggle(axiom: 7, state: sender.state)
-    }
-
-    func toggle(axiom: Int, state: Int) {
-        main.useAxioms[axiom - 1] = state == 0 ? false : true
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
+    func toggleAxiom(_ sender: NSButton) {
+        main.useAxioms[sender.tag - 1] = sender.state == 0 ? false : true
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
+    }
+
+    func setUpView() {
+        let paperDimensionsText = NSTextField()
+        paperDimensionsText.stringValue = "Paper Dimensions"
+        setUp(textField: paperDimensionsText)
+        paperDimensionsText.font = NSFont.boldSystemFont(ofSize: 13)
+        paperDimensionsText.alignment = .center
+
+        let useSquareButton = NSButton(
+            checkboxWithTitle: "Square",
+            target: nil,
+            action: nil
+        )
+        useSquareButton.bind(
+            "value",
+            to: self,
+            withKeyPath: "useSquare",
+            options: nil
+        )
+
+        let aspectRatioText = NSTextField()
+        let aspectRatioColon = NSTextField()
+        aspectRatioText.stringValue = "Aspect Ratio"
+        aspectRatioColon.stringValue = ":"
+        setUp(textField: aspectRatioText)
+        setUp(textField: aspectRatioColon)
+        widthInput  = TextField()
+        heightInput = TextField()
         widthInput.delegate  = self
         heightInput.delegate = self
-        maxRankSelector.removeAllItems()
+        widthInput.target  = self
+        heightInput.target = self
+        widthInput.action = #selector(ConfigurationViewController.updateWidth)
+        heightInput.action = #selector(ConfigurationViewController.updateHeight)
+        widthInput.bind(
+            "enabled",
+            to: self,
+            withKeyPath: "useSquare",
+            options: [
+                NSValueTransformerNameBindingOption:
+                    NSValueTransformerName.negateBooleanTransformerName
+            ]
+        )
+        heightInput.bind(
+            "enabled",
+            to: self,
+            withKeyPath: "useSquare",
+            options: [
+                NSValueTransformerNameBindingOption:
+                    NSValueTransformerName.negateBooleanTransformerName
+            ]
+        )
+        useSquare = true
+        let aspectRatioStack = NSStackView(
+            views: [aspectRatioText, widthInput, aspectRatioColon, heightInput]
+        )
+        widthInput.translatesAutoresizingMaskIntoConstraints  = false
+        heightInput.translatesAutoresizingMaskIntoConstraints = false
+        widthInput.widthAnchor.constraint(equalToConstant: 100).isActive  = true
+        heightInput.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        aspectRatioStack.orientation = .horizontal
+
+        let axiomsText = NSTextField()
+        axiomsText.stringValue = "Axioms"
+        setUp(textField: axiomsText)
+        let axiomsStack = NSStackView()
+        axiomsStack.orientation = .horizontal
+        axiomsStack.addArrangedSubview(axiomsText)
+        for i in 1...7 {
+            let axiomButton = NSButton(
+                checkboxWithTitle: String(i),
+                target: self,
+                action: #selector(ConfigurationViewController.toggleAxiom(_:))
+            )
+            axiomButton.state = 1
+            axiomButton.tag = i
+            axiomsStack.addArrangedSubview(axiomButton)
+            if i == 7 {
+                axiomButton.translatesAutoresizingMaskIntoConstraints = false
+                axiomButton.rightAnchor
+                           .constraint(equalTo: axiomsStack.rightAnchor)
+                           .isActive = true
+            }
+        }
+
+        let maxRankText = NSTextField()
+        maxRankText.stringValue = "Maximum Rank"
+        setUp(textField: maxRankText)
+        let maxRankSelector = NSPopUpButton(
+            title: "",
+            target: self,
+            action: #selector(ConfigurationViewController.setMaxRank(_:))
+        )
         for i in 1...6 {
             maxRankSelector.addItem(withTitle: String(i))
         }
         maxRankSelector.selectItem(at: main.maxRank - 1)
+        let maxRankStack = NSStackView(
+            views: [maxRankText, maxRankSelector]
+        )
+        maxRankSelector.setContentHuggingPriority(249, for: .horizontal)
+        maxRankStack.orientation = .horizontal
+
+        errorMessage = NSTextField()
+        setUp(textField: errorMessage)
+
+        let reinitializeButton = NSButton(
+            title: "Reinitialize",
+            target: self,
+            action: #selector(ConfigurationViewController.reinitialize)
+        )
+        reinitializeButton.bind(
+            "enabled",
+            to: self,
+            withKeyPath: "isWidthValid",
+            options: nil
+        )
+        reinitializeButton.bind(
+            "enabled2",
+            to: self,
+            withKeyPath: "isHeightValid",
+            options: nil
+        )
+        reinitializeButton.bind(
+            "enabled3",
+            to: self,
+            withKeyPath: "delegate.hasFinishedInitialization",
+            options: nil
+        )
+        reinitializeButton.bind(
+            "enabled4",
+            to: self,
+            withKeyPath: "isEditing",
+            options: [
+                NSValueTransformerNameBindingOption:
+                    NSValueTransformerName.negateBooleanTransformerName
+            ]
+        )
+        reinitializeButton.setContentHuggingPriority(261, for: .horizontal)
+
+        let stackView = NSStackView(
+            views: [
+                paperDimensionsText,
+                useSquareButton,
+                aspectRatioStack,
+                axiomsStack,
+                maxRankStack,
+                errorMessage,
+                reinitializeButton,
+            ]
+        )
+        stackView.orientation = .vertical
+        stackView.alignment = .left
+
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.leftAnchor
+                     .constraint(equalTo: view.leftAnchor, constant: 20),
+            stackView.rightAnchor
+                     .constraint(equalTo: view.rightAnchor, constant: -20),
+            stackView.topAnchor
+                     .constraint(equalTo: view.topAnchor, constant: 20),
+        ])
+
+        paperDimensionsText.translatesAutoresizingMaskIntoConstraints = false
+        paperDimensionsText.centerXAnchor
+                           .constraint(equalTo: stackView.centerXAnchor)
+                           .isActive = true
+
+        aspectRatioStack.translatesAutoresizingMaskIntoConstraints = false
+        aspectRatioStack.rightAnchor
+                        .constraint(equalTo: stackView.rightAnchor)
+                        .isActive = true
+
+        axiomsStack.translatesAutoresizingMaskIntoConstraints = false
+        axiomsStack.rightAnchor
+                   .constraint(equalTo: stackView.rightAnchor)
+                   .isActive = true
+
+        maxRankStack.translatesAutoresizingMaskIntoConstraints = false
+        maxRankStack.rightAnchor
+                    .constraint(equalTo: stackView.rightAnchor)
+                    .isActive = true
+
+        reinitializeButton.translatesAutoresizingMaskIntoConstraints = false
+        reinitializeButton.rightAnchor
+                          .constraint(equalTo: stackView.rightAnchor)
+                          .isActive = true
+    }
+
+    func setUp(textField: NSTextField) {
+        textField.font = NSFont.systemFont(ofSize: 13)
+        textField.isSelectable = false
+        textField.drawsBackground = false
+        textField.isBordered = false
     }
 }
